98534993b7f9c8c1d3ce16e81a65071cc861b941
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2023-08-22 12:38:24 +0530

    Check_for_prime.java

---
+++ 6. Loops/16.Check_for_prime.java
@@ -0,0 +1,35 @@
+package Loops;
+
+import java.util.*;
+
+class Check_for_prime {
+
+    static void checkforPrime(int num) {
+        boolean flag = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                flag = false;
+            }
+        }
+
+        if (num > 0 && flag) {
+            System.out.println("The number is prime");
+        } else {
+            System.out.println("The number is not a prime number");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        checkforPrime(num);
+    }
+}
+
+// output:
+// 41
+// The number is prime
+
+// 49
+// The number is not a prime number
