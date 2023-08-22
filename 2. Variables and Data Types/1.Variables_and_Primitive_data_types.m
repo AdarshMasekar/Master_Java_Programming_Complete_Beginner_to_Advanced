91ebe386a2470b8cae4dcf786129f6caf1d13e43
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2023-08-22 11:00:47 +0530

    variables and data types

---
+++ 2. Variables and Data Types/1.Variables_and_Primitive_data_types.m
@@ -0,0 +1,18 @@
+variables in java are the containers used to store and retrieve data
+
+variable is a name given to the storage space
+
+in data types there are 2 types: '\n"
+
+1. Primitive "\n"
+2. Non-Primitive "\n"
+
+java has 8 built in primitive data types : "\n"
+byte "\n"
+short "\n"
+int "\n"
+long "\n"
+float "\n"
+double "\n"
+char "\n"
+boolean "\n"
