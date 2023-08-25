a6a2979260cc1c930014ed5d8067683b233d7b3f
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2023-08-26 00:07:52 +0530

    Intro_to_ArrayList.java

---
+++ 11. ArrayList/01. Intro_to_ArrayList.java
@@ -0,0 +1,21 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Intro_to_ArrayList {
+    public static void main(String[] args) {
+        // Create an ArrayList to store integers
+        ArrayList<Integer> al = new ArrayList<>();
+
+        // Add elements to the ArrayList
+        al.add(10);
+        al.add(20);
+        al.add(30);
+
+        // Print the contents of the ArrayList
+        System.out.println(al);
+    }
+}
+
+// output:
+// [10,20,30]
