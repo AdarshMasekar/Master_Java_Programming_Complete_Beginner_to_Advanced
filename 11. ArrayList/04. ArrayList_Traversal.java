38e0061a3f0d32daf6dffde449bfee50d156dd84
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2023-08-26 01:53:41 +0530

    ArrayList_Traversal.java

---
+++ 11. ArrayList/04. ArrayList_Traversal.java
@@ -0,0 +1,86 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayList_Traversal {
+
+    // Using traditional for loop and get() method
+    static void usingForLoop(ArrayList<String> al) {
+        for (int i = 0; i < al.size(); i++) {
+            System.out.print(al.get(i) + " ");
+        }
+    }
+
+    // Using enhanced for-each loop
+    static void usingForEachLoop(ArrayList<String> al) {
+        for (String name : al) {
+            System.out.print(name + " ");
+        }
+    }
+
+    // Using Iterator
+    static void usingIterator(ArrayList<String> al) {
+        java.util.Iterator<String> it = al.iterator();
+        while (it.hasNext()) {
+            String name = it.next();
+            if (name.length() < 5) {
+                it.remove(); // Removes short names from the ArrayList
+            } else {
+                System.out.print(name + " ");
+            }
+        }
+    }
+
+    // Using forEach method
+    static void usingForEachMethod(ArrayList<String> al) {
+        al.forEach(name -> {
+            System.out.print(name + " ");
+        });
+    }
+
+    public static void main(String[] args) {
+        // There are a total of 4 ways to traverse an ArrayList
+
+        ArrayList<String> al = new ArrayList<>();
+        al.add("adarsh");
+        al.add("akash");
+        al.add("vicky");
+        al.add("raghu");
+        al.add("manju");
+        al.add("somu");
+        al.add("pandari");
+
+        System.out.println("Printed using for loop and get() method:");
+        usingForLoop(al);
+        System.out.println("\n");
+
+        System.out.println("Printed using enhanced for-each loop:");
+        usingForEachLoop(al);
+        System.out.println("\n");
+
+        System.out.println("Printed using Iterator:");
+        usingIterator(al);
+        System.out.println("\n");
+
+        System.out.println("Printed using forEach method:");
+        usingForEachMethod(al);
+        System.out.println("\n");
+    }
+}
+
+// output:
+
+// Printed using for
+// loop and
+
+// get() method:
+// adarsh akash vicky raghu manju somu pandari
+
+// Printed using enhanced for-each loop:
+// adarsh akash vicky raghu manju somu pandari
+
+// Printed using Iterator:
+// adarsh akash vicky raghu manju pandari
+
+// Printed using forEach method:
+// adarsh akash vicky raghu manju pandari
