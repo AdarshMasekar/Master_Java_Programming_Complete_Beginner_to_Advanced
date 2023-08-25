db874eea9d5726490f39204e0ad60361417572e6
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2023-08-25 16:07:49 +0530

    Reverse_String.java
    added Stringbuilder method

--- 09. Strings/10. Reverse_String.java
+++ 09. Strings/10. Reverse_String.java
@@ -6,19 +6,40 @@ public class Reverse_String {
     // Function to reverse a given string and print the reversed string
     static void reverse(String str) {
         System.out.print("Reversed string is: ");
+
+        // Iterate through the characters of the input string in reverse order
         for (int i = str.length() - 1; i >= 0; i--) {
             // Print the character at index i, starting from the end of the string
             System.out.print(str.charAt(i));
         }
+
+        // No need to store the reversed string if we're just printing it
+    }
+
+    // Function to reverse a string using the StringBuilder class
+    static void reverseUsingStringBuilder(String str) {
+        // Create a StringBuilder instance with the input string
+        StringBuilder sb = new StringBuilder(str);
+
+        // Use the reverse() method of StringBuilder to reverse the content
+        sb.reverse();
+
+        // Print the reversed string obtained from the StringBuilder
+        System.out.println(sb.toString());
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter the string to be reversed: ");
         String str = sc.nextLine();
 
         // Call the reverse function to reverse and print the input string
         reverse(str);
+
+        // Call the reverseUsingStringBuilder function to achieve the same using
+        // StringBuilder
+        reverseUsingStringBuilder(str);
 
         // Close the scanner to free up resources
         sc.close();
