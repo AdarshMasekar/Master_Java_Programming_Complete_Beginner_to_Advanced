875ba2d2c2f8ad9e319d070a0066503231bf7a0e
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2023-09-04 22:53:03 +0530

    To_Array_Methods.java

---
+++ 06. To_Array_Methods.java
@@ -0,0 +1,36 @@
+package Generics;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class To_Array_Methods {
+
+    public static void main(String[] args) {
+        // Create a Set of integers called "list" using a TreeSet with reverse ordering
+        // comparator.
+        Set<Integer> list = new TreeSet<>(Comparator.reverseOrder());
+
+        // Add three integers to the set.
+        list.add(20);
+        list.add(10);
+        list.add(30);
+
+        // method one: Convert the Set to an Object array and print its elements.
+        Object[] arr1 = list.toArray();
+        for (Object x : arr1) {
+            System.out.print(x + " ");
+        }
+
+        // Create an Integer array with the same size as the Set and populate it with
+        // Set elements.
+        Integer[] arr2 = new Integer[list.size()];
+        arr2 = list.toArray(arr2);
+
+        // Print the elements of the Integer array.
+        for (Integer x : arr2) {
+            System.out.print(x + " ");
+        }
+    }
+}
