3ec249624d2c4a924eaa24691a6faf578b59f2f1
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2023-10-03 00:00:19 +0530

    Intro to arraylist

---
+++ 14. List/ArrayList/01. Basic_ArrayList.java
@@ -0,0 +1,21 @@
+package List.ArrayList;
+
+import java.util.ArrayList;
+
+public class Basic_ArrayList {
+    public static void main(String[] args) {
+        // Create a new ArrayList to store integers
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // Add elements to the list
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        // printint the elements of the array
+        System.out.println(list);// [10, 20, 30, 40, 50]
+
+    }
+}
