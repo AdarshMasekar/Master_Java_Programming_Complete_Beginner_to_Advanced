45d7b59ce7a8e26b73714b06eb394f348255f4e5
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2023-10-26 23:46:03 +0530

    insert node at the beginning of the linkedlist

---
+++ 14. List/LinkedList/02. InsertAtBeginning.java
@@ -0,0 +1,24 @@
+package List.LinkedList;
+
+public class InsertAtBeginning extends CustomLinkedList {
+
+    public void addFront(int data) {
+        Node newest = new Node(data);
+        newest.next = head;
+        head = newest;
+    }
+
+    public static void main(String[] args) {
+        InsertAtBeginning cl = new InsertAtBeginning();
+        cl.addLast(50);
+        cl.addLast(60);
+        cl.addLast(70);
+        cl.addFront(40);
+        cl.addFront(30);
+        cl.addFront(20);
+        cl.addFront(10);
+
+        cl.display();
+
+    }
+}
